b48610be8af419c5763b2e9d7e7c03cd8fa1d941
krutantsinh <krutantsinh@gmail.com>
2023-09-26 15:46:09 +0530

    updated files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,17 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      strokeWidth: {
+        '1.5': '1.5px',
+      },
+      backgroundImage: {
+        'avatar': "url('/public/images/User_avatar.png')",
+        'search': "url('/public/images/search.svg')",
+        'x': "url('/public/images/signup-x.svg')",
+        'tweetavatar': "url('/public/images/tweet-avatar.png')",
+
+
+      },
       borderRadius: {
         '65': '65px',
         '30': '30px',
@@ -15,6 +26,7 @@ module.exports = {
         sm: '1rem',
         12: ['12px','24px'],
         14: ['14px','24px'],
+        15: ['15px','24px'],
         26: '1.625rem',
         20: ['20px', '24px'], 
         48: ['48px', '24px'],
@@ -64,11 +76,20 @@ module.exports = {
         '434': '434px',
         '482': '482px',
         '598': '598px',
+        '348': '348px',
+        '19' : '19px',
+        '512':'512px',
+        '14.564':'14.564px',
 
       },
       height: {
         '58': '58px',
         '3': '3px',
+        '39': '39px',
+        '19' : '19px',
+        '143': '143px',
+        '355':'355px',
+        '14.564':'14.564px',
       },
       borderColor: {
         'color': 'var(--stroke, rgba(29, 155, 240, 0.24))',
@@ -76,6 +97,8 @@ module.exports = {
       },
       spacing: {
         '100': '6.25rem',
+        '15': '15px',
+        '5': '5px',
       },
       padding: {
         '236.5': '14.781rem',
@@ -83,6 +106,12 @@ module.exports = {
         '15': '0.938rem',
         '93': '5.813rem',
         '5': '5px',
+        '247': '247px',
+        '1.583': '1.583px',
+        '1.782':'1.782px',
+        '1.781':'1.781px',
+        '1.583':'1.583px',
+        '15': '15px',
       }
   },
   plugins: [],
